b27537d8bcbe594ab5fbc8c523e55b4d08b29c8f
b11005 <instantjoy3@gmail.com>
2015-06-25 16:34:15 +0900

    1e-8

--- testMatrix.java
+++ testMatrix.java
@@ -7,9 +7,9 @@ public class testMatrix {
 
 	@Test
 	public void test() {
-		assertEquals(Matrix.main(3), 450.0, 1e-99);
-		assertEquals(Matrix.main(4), 3680.0, 1e-99);
-		assertEquals(Matrix.main(5), 18250.0, 1e-99);
+		assertEquals(Matrix.main(3), 450.0, 1e-8);
+		assertEquals(Matrix.main(4), 3680.0, 1e-8);
+		assertEquals(Matrix.main(5), 18250.0, 1e-8);
 		assertEquals(Matrix.main(300),54673987504423472.000000, 1e-8);
 	}
 
