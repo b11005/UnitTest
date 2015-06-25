ef9291c7c499b8480064488d39c28db7dbb62dcf
b11005 <instantjoy3@gmail.com>
2015-06-25 16:34:38 +0900

    add array

--- IsOpenNow/src/isOpenNow.java
+++ IsOpenNow/src/isOpenNow.java
@@ -14,24 +14,23 @@ public class isOpenNow {
                     = new BufferedReader(new FileReader(file));
 
                 String str, name, tmp, ampm;
-				int close, ohour,omin,chour,cmin;
+				int data[]=new int[5];
 				String[] str1, temp, temp1;
                 while((str = br.readLine()) != null){
                     System.out.println(str);
                     str1=str.split("\t");
                     name=str1[0];//店名
-                    close=Integer.parseInt(str1[1]);//定休日
+                    data[0]=Integer.parseInt(str1[1]);//定休日
                     tmp=str1[2];
                     temp=tmp.split("[: ~]");
-                    ohour=Integer.parseInt(temp[0]);//開店時間
-                    omin=Integer.parseInt(temp[1]);//開店　分
+                    data[1]=Integer.parseInt(temp[0]);//開店時間
+                    data[2]=Integer.parseInt(temp[1]);//開店　分
                     temp1=temp[2].split("[M]");
-                    chour=Integer.parseInt(temp1[1]);//閉店時間
-                    System.out.println(chour);
+                    data[3]=Integer.parseInt(temp1[1]);//閉店時間
                     ampm=temp1[0];
                     System.out.println(ampm);
-                    cmin=Integer.parseInt(temp[3]);//閉店　分
-                    getDate(close, ohour, omin, chour, cmin,ampm);
+                    data[4]=Integer.parseInt(temp[3]);//閉店　分
+                    getDate(data,ampm);
                 }
 
                 br.close();
@@ -55,24 +54,23 @@ public class isOpenNow {
         return false;
     }
 
-	public static boolean IsOpenNow(int dayOfWeek, int hour, int close, int ohour, int chour, String ampm) {
-	    if (dayOfWeek == close){
+	public static boolean IsOpenNow(int dayOfWeek, int hour, int[] data, String ampm) {
+	    if (dayOfWeek == data[0]){
 	    	System.out.println("Close now");
 	    	return false;
-	    }else if(ampm=="AM" && hour<chour){
+	    }else if(ampm=="AM" && hour<data[3]){
 	    	System.out.println("Open now");
 	    	return true;
 	    }
 	    System.out.println("Open now");
-	    return ohour <= hour && hour < chour;
-	  }//int close, int ohour, int omin, int chour, int cmin
-	//int dayOfWeek, int hour, int close, int ohour, int chour
+	    return data[1] <= hour && hour < data[3];
+	  }
 	  
-	  public static boolean getDate(int close, int ohour, int omin, int chour, int cmin, String ampm) {
+	  public static boolean getDate(int[] data, String ampm) {
 	    Calendar calendar = Calendar.getInstance();
 	    int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 	    int hour = calendar.get(Calendar.HOUR_OF_DAY);
-	    return IsOpenNow(dayOfWeek, hour,close, ohour, chour, ampm);
+	    return IsOpenNow(dayOfWeek, hour, data, ampm);
 	  }
 }
 
